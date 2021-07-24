f4aa99ff8751cbfc312ad6034e3cb1c1a7730e41
Yauheni.Kaliada <yauheni.kaliada@softesity.com>
2021-07-24 23:05:48 +0300

    Init commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { AppContainer } from './styles';
+
+const App = () => {
+  return (
+    <AppContainer>
+      Test
+    </AppContainer>
+  );
+}
+
+export default App;
